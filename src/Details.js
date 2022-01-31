ce1b59ac57623acfbd853d30b90be4d0fe84f461
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-01-31 18:43:07 -0500

    feat(class-component): Implement Error boundary class component and wrap details component with it

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
   state = { loading: true };
@@ -21,6 +22,7 @@ class Details extends Component {
     const { animal, breed, city, state, description, name, images } =
       this.state;
 
+    throw new Error("Something wrong");
     return (
       <div>
         <h1>{name}</h1>
@@ -33,4 +35,13 @@ class Details extends Component {
   }
 }
 
-export default withRouter(Details);
+//export default withRouter(Details);
+const DetailsWithRouter = withRouter(Details);
+
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <DetailsWithRouter {...props} />
+    </ErrorBoundary>
+  );
+}
