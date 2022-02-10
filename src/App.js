1bc33e222d944bd44898885c7efb9f6848fd22e2
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-02-09 20:11:19 -0500

    feat(code-splitting): Lazy load details, search,hooks, and modal components

--- src/App.js
+++ src/App.js
@@ -1,58 +1,62 @@
-import { useState, StrictMode } from "react";
+import { useState, StrictMode, lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Search from "./Search";
-import Details from "./Details";
 import ThemeContext from "./ThemeContext";
-import StateComponent from "./components/StateComponent";
-import EffectComponent from "./components/EffectComponent";
-import ContextComponent from "./components/ContextComponent";
-import RefComponent from "./components/RefComponent";
-import ReducerComponent from "./components/ReducerComponent";
-import MemoComponent from "./components/MemoComponent";
-import CallbackComponent from "./components/CallbackComponent";
+
+const StateComponent = lazy(() => import("./components/StateComponent"));
+const EffectComponent = lazy(() => import("./components/EffectComponent"));
+const ContextComponent = lazy(() => import("./components/ContextComponent"));
+const RefComponent = lazy(() => import("./components/RefComponent"));
+const ReducerComponent = lazy(() => import("./components/ReducerComponent"));
+const MemoComponent = lazy(() => import("./components/MemoComponent"));
+const CallbackComponent = lazy(() => import("./components/CallbackComponent"));
+const Details = lazy(() => import("./Details"));
+const Search = lazy(() => import("./Search"));
+
 const App = () => {
   const theme = useState("blue");
 
   return (
     <ThemeContext.Provider value={theme}>
       <div>
-        <Router>
-          <header style={{ textAlign: "center", marginBottom: "5rem" }}>
-            <Link style={{ textDecoration: "none", color: "red" }} to="/">
-              <h1>Acquire new Pet</h1>
-            </Link>
-          </header>
-          <Switch>
-            <Route path="/details/:id">
-              <Details />
-            </Route>
-            <Route path="/state">
-              <StateComponent />
-            </Route>
-            <Route path="/effect">
-              <EffectComponent />
-            </Route>
-            <Route path="/context">
-              <ContextComponent />
-            </Route>
-            <Route path="/ref">
-              <RefComponent />
-            </Route>
-            <Route path="/reducer">
-              <ReducerComponent />
-            </Route>
-            <Route path="/memo">
-              <MemoComponent />
-            </Route>
-            <Route path="/callback">
-              <CallbackComponent />
-            </Route>
-            <Route path="/">
-              <Search />
-            </Route>
-          </Switch>
-        </Router>
+        <Suspense fallback="<h1>loading route...</h1>">
+          <Router>
+            <header style={{ textAlign: "center", marginBottom: "5rem" }}>
+              <Link style={{ textDecoration: "none", color: "red" }} to="/">
+                <h1>Acquire new Pet</h1>
+              </Link>
+            </header>
+            <Switch>
+              <Route path="/details/:id">
+                <Details />
+              </Route>
+              <Route path="/state">
+                <StateComponent />
+              </Route>
+              <Route path="/effect">
+                <EffectComponent />
+              </Route>
+              <Route path="/context">
+                <ContextComponent />
+              </Route>
+              <Route path="/ref">
+                <RefComponent />
+              </Route>
+              <Route path="/reducer">
+                <ReducerComponent />
+              </Route>
+              <Route path="/memo">
+                <MemoComponent />
+              </Route>
+              <Route path="/callback">
+                <CallbackComponent />
+              </Route>
+              <Route path="/">
+                <Search />
+              </Route>
+            </Switch>
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
